b9c1db0cc6e09e404cc54eb10ed82153cb3a8f3c
ALAWI-VPN <159485644+ALAWI-VPN@users.noreply.github.com>
2024-02-14 17:30:06 +0700

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #pengambilan repository
 wget https://raw.githubusercontent.com/AlawiStore24/domain/main/pointingtes.sh
-
 #pindah ke /usr/bin/
 mv pointingtes.sh /usr/bin/pointing
+#izin akses
 cd /usr/bin/
 chmod +x pointing
+#kembali ke root
 cd
+rm -rf tes.sh
+#meminta persetujuan mulai pointing
+read -rp "Tekan [ Enter ] untuk memulai pointing... "
 pointing
